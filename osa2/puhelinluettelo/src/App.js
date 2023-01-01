4813b30b078e608b419785c6bfe9272ddea85ae5
Jáx <jx@pop-os.localdomain>
2023-01-01 18:54:48 +0200

    1.1.23

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,75 +1,73 @@
-import {useState, useEffect} from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react'
+import Grud from './services/hooks'
 
-const Filter = ({filter, setFilter}) => {
-  const FilterFunc = (event) => {
-    setFilter(event.target.value)
-  }  
-  return ( 
-      <form>
-        <div>
-          filter show with<input value={filter} onChange={FilterFunc}/>
-        </div>
-      </form>
-  )
-}
-
-const PersonForm = ({data, setData}) => {
-  const [name, setName] = useState('')
-  const [number, setNumber] = useState('')
-  const NameField = (event) => {
-    setName(event.target.value)  
-  }
-  const NumberField = (event) => {
-    setNumber(event.target.value) 
-  }
-  const addNumber = (event) => {
-    let name_reserver = false
-    data.forEach(element => {
-      if (element.name === name) {
-        name_reserver = true
-        alert(`${name} is reserved`)
-      }
-    });
-    if (name_reserver === false) {
-      event.preventDefault()
-      const personObject = {
-        name: name,
-        number: number,
-        id: data.length + 1,
-      }
-      setData(data.concat(personObject))
-    }
-  }
+const Filter = ({ filter, setFilter }) => {
   return (
     <form>
-      <div>name:<input value={name} onChange={NameField}/></div>
-      <div>number:<input value={number} onChange={NumberField}/></div>
-      <button onClick={addNumber}>add</button>
+      filter shown with <input value={filter} onChange={event => setFilter(event.target.value)} />
     </form>
   )
 }
 
-const Persons = ({data, filter}) => {
-  if (filter === "") {
-    return (
-      <div>
-        {data.map(element => 
-          <p key={element.id}>{element.name} {element.number}</p>  
-        )}
-      </div>
-    )
-  } else {
-    const filtered = [] 
+const PersonForm = ({ data }) => {
+  const [username, setUsername] = useState('')
+  const [number, setNumber] = useState('')
+
+  const handleAdd = (event) => {
+    if (username != '' && number != '') {
+      event.preventDefault()
+      const newObject = {
+        name: username,
+        number: number,
+        id: (data.length + 1),
+      }
+      Grud.create(newObject)
+    }
+  }
+
+  return (
+    <form>
+      name: <input value={username} onChange={event => setUsername(event.target.value)} />
+      <br />
+      number: <input value={number} onChange={event => setNumber(event.target.value)} />
+      <br />
+      <button onClick={(event) => handleAdd(event)}>add</button>
+    </form>
+  )
+}
+
+const Persons = ({ data, filter }) => {
+
+  const remove = (event, person) => {
+    event.preventDefault()
+    if (window.confirm("Delete?")) {
+      Grud.remove(person)
+    }
+  }
+
+  if (filter != '') {
+    const filtered_list = []
     data.forEach(element => {
       if (element.name.match(filter)) {
-        filtered.push(element)
+        filtered_list.push(element)
       }
     });
     return (
       <div>
-        {filtered.map(element => 
-          <p key={element.id}>{element.name} {element.number}</p>  
+        {filtered_list.map(person =>
+            <p key={person.id}>{person.name} {person.number} 
+            <button onClick={(event) => remove(event, person.id)}>delete</button>
+            </p>
+        )}
+      </div>
+      )
+  } else {
+    return (
+      <div>
+        {data.map(person =>
+            <p key={person.id}>{person.name} {person.number} 
+            <button onClick={(event) => remove(event, person.id)}>delete</button>
+            </p>
         )}
       </div>
     )
@@ -77,33 +75,30 @@ const Persons = ({data, filter}) => {
 }
 
 const App = () => {
-  const URL = 'http://localhost:3001/persons'
-  const [data, setData] = useState([])
   const [filter, setFilter] = useState('')
+  const [persons, setPersons] = useState([])
+  const [count, setCount] = useState(0)
+
   useEffect(() => {
-  axios
-    .get(URL)
-    .then(response => {
-      setData(response.data)
-    })
-  }, [])
+    Grud.getAll()
+      .then(res => setPersons(res.data))
+  });
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter
-      filter={filter} 
-      setFilter={setFilter}
+        filter={filter}
+        setFilter={setFilter}
       />
       <h3>Add a new</h3>
       <PersonForm 
-      data={data}
-      setData={setData}
+        data={persons}
       />
       <h3>Numbers</h3>
       <Persons
-      data={data}
-      filter={filter}
+        data={persons}
+        filter = {filter}
       />
     </div>
   )
